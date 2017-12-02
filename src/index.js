d41e45d54694db2e2daa3f5a45f7c9b008b84f9e
Matias Arriola <matias.arriola@gmail.com>
2017-12-02 11:45:38 -0300

    Add Standard
    Added `lint` and `lint-fix` scripts
    Fixed existing code

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import {Provider} from 'react-redux';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+import {Provider} from 'react-redux'
 import configureStore from './redux/store'
 
 ReactDOM.render(
   <Provider store={configureStore()}>
     <App />
   </Provider>, document.getElementById('root')
-);
+)
 
-registerServiceWorker();
+registerServiceWorker()
